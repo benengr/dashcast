97fe7676a3d8d7f24c16ef606bb7b373ad747945
Ben S. Anderson <ben.anderson@assurant.com>
2018-12-11 17:36:29 -0500

    getting config from server

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,48 @@
 import React, { Component } from 'react';
 import ReactLoading from 'react-loading';
 import './App.css';
+import axios from 'axios';
 
 class App extends Component {
 
-  render() {
+  constructor(props) {
+    super(props);
+    this.state = null;
+    this.get_config();
+  }
+
+  get_config() {
+    axios.get('/dash_manager/v1/boards').then(res => {
+      console.log(res);
+      this.setState({config: res.data});
+    });
+  }
+
+  render_loading() {
     return (
       <div className="App">
         <ReactLoading color="#888888" height="25%" width="25%" />
       </div>
     );
+  }
+
+  render_config() {
+    return (
+      <div className="App">
+          {this.state.config.toString()}
+      </div>
+    );
+  }
+
+  render() {
+    if(this.state === null) {
+      console.log("Waiting for staet")
+      return this.render_loading();
+    } else {
+      console.log("Have config");
+      return this.render_config();
+    }
+    
   }
 }
 
