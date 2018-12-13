4377d891bcf1fe8d2be931fc35001ee69e8555df
Ben S. Anderson <ben.anderson@assurant.com>
2018-12-13 14:34:13 -0500

    refactor to handle sizing of url array

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,23 +7,36 @@ class App extends Component {
 
   constructor(props) {
     super(props);
-    this.state = null;
+    this.state = {
+      boardList: null,
+      delay: null
+    };
     this.get_config();
   }
 
   get_config() {
     axios.get('/dash_manager/v1/boards').then(res => {
       console.log(`Setting state to ${JSON.stringify(res.data)}`);
-      this.setState({config: res.data});
+      this.setState({
+        boardList: res.data.boardList,
+        delay: res.data.delay
+      });
     });
+  }
+
+  handleUrlChange(event, index) {
+    const newUrls = this.state.boardList.map((url, idx) => {
+      if(idx !== index) return url;
+      return(event.target.value);
+    });
+
+    this.setState({boardList: newUrls});
   }
 
   render_urls(urls) {
     return urls.map((url, index) => {
       return (
-        <div className="form-group" key={index}>
-          <input className="form-control" type="text" defaultValue={url}/>
-        </div>
+        <input className="form-control" key={index} type="text" value={url} onChange={(e) => {this.handleUrlChange(e, index)}} />
       );
     });
   }
@@ -37,22 +50,31 @@ class App extends Component {
   }
 
   render_config() {
-    console.info(`config is: ${JSON.stringify(this.state.config)}`);
+    console.info(`state is: ${JSON.stringify(this.state)}`);
     return (
       <div className="App">
         <form>
-          { this.render_urls(this.state.config.boardList) }
+          { this.render_urls(this.state.boardList) }
+          
           <div className="form-group">
             <label htmlFor="delayInput">Delay</label>
-            <input type="number" className="form-control" id="delayInput" defaultValue={this.state.config.delay} />
+            <input type="number" className="form-control" id="delayInput" defaultValue={this.state.delay} />
+
           </div>
+          <span className="glyphicon glyphicon-search" aria-hidden="true"></span>
+          <button type="button" className="btn btn-default">
+            Add
+          </button>
+          <button type="button" className="btn btn-default">
+            <span className="glyphicon glyphicon-align-left" aria-hidden="true"></span>
+          </button>
         </form>
       </div>
     );
   }
 
   render() {
-    if(this.state === null) {
+    if(this.state.boardList === null) {
       console.log("Waiting for state")
       return this.render_loading();
     } else {
